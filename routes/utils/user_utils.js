97139027a28d1b3d9320ab6529c375db439ef4ad
HodayaTakele <hodayat@post.bgu.ac.il>
2022-06-14 23:58:46 +0300

    almost finish

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -18,6 +18,38 @@ async function getLastWatchedRecipes(user_id) {
     return recipes_id[0];
 }
 
+// async function addUserRecipe(recipeDetails, username) {
+//     const {
+//         title,
+//         image,
+//         readyInMinutes,
+//         glutenFree,
+//         isVegaterian,
+//         ingredients,
+//         instructions,
+//         servings,
+//         summary
+//     } = recipeDetails
+
+//     //Add new recipe to DB
+//     pool = await poolPromise
+//     result = await pool.request()
+//         .input("user_id", sql.VarChar(10), user_id)
+//         .input("title", sql.VarChar(4000), title)
+//         .input("image", sql.VarChar(4000), image)
+//         .input("readyInMinutes", sql.BigInt, readyInMinutes)
+//         .input("aggregateLikes", sql.BigInt, 0)
+//         .input("glutenFree", sql.Bit, glutenFree === 'true' ? 1 : 0)
+//         .input("vegetarian", sql.Bit, isVegaterian === 'true' ? 1 : 0)
+//         .input("ingredients", sql.NVarChar('max'), JSON.stringify(ingredients))
+//         .input("instructions", sql.NVarChar('max'), JSON.stringify(instructions))
+//         .input("servings", sql.NVarChar('max'), servings)
+//         .input("summary", sql.NVarChar(4000), summary)
+//         .execute("insertRecipe").then(function (recordSet) {
+//             res.status(200).send({ message: 'Succsefully created a new recipe', sucess: 'true' })
+//         })
+// }
+
 exports.getLastWatchedRecipes = getLastWatchedRecipes;
 exports.markAsLastWatched = markAsLastWatched;
 exports.markAsFavorite = markAsFavorite;
